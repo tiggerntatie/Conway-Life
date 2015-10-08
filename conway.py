aba2778cae270d2484c9cc5cdb8386e7e9d2d26d
Eric Dennison <ericd@netdenizen.com>
2015-10-07 21:13:35 -0400

    Updated from Brython Server: 10/7/2015 9:13:35 PM

--- conway.py
+++ conway.py
@@ -6,5 +6,32 @@ Assignment:
 Write and submit a program that plays Conway's Game of Life, per 
 https://github.com/HHS-IntroProgramming/Conway-Life
 """
-from ggame import App, Sprite, CircleAsset
+from ggame import App, Sprite, CircleAsset, Color
 
+red = Color(0xff0000, 1.0)
+blue = Color(0x0000ff, 1.0)
+black = Color(0, 1.0)
+noline = LineStyle(0, black)
+CELLDIAMETER = 10
+CELLSWIDE = 100
+CELLSHIGH = 50
+
+class Cell(Sprite):
+    """
+    Cell that is either new or old.
+    Pass True for new if it shall be a new color.
+    """
+    bluecircle = CircleAsset(4, noline, blue)
+    redcircle = CircleAsset(4, noline, blue)
+
+    def __init__(self, new, pos):
+        asset = Cell.bluecircle
+        if new:
+            asset = Cell.redcircle
+        super.__init__(asset, pos)
+        self.address = (None, None)
+        self.visible = False
+        
+    def setAddress(self, x, y):
+        self.address = (x, y)
+        
